829be54f87fc5a6da175f0eaef586403876cab81
BrouillardLiam <liambrouillard@outlook.fr>
2023-05-04 09:51:17 -0400

    Atelier 11: Mise en place notification, avant correction erreur de compil

--- src/app/probleme/probleme.component.ts
+++ src/app/probleme/probleme.component.ts
@@ -13,12 +13,18 @@ export class ProblemeComponent implements OnInit{
   problemeForm: FormGroup;
   typesProbleme:ITypeProbleme[]
   errorMessage: string;
+  notification: string;
+  isNotificationEnabled: boolean;
   constructor(private fb: FormBuilder, private typeproblemeService:TypeProbleme ) {
     this.problemeForm = this.fb.group({
       prenom: ['', [VerifierCaracteresValidator.longueurMinimum(3),Validators.required]],
       nom: ['',[Validators.required, Validators.maxLength(50)]],
       typeprobleme: [''],
-      
+      courrielGroup: this.fb.group({
+        courriel: [{value: '', disabled: true}],
+        courrielConfirmation: [{value: '', disabled: true}],
+      }),
+    telephone: [{value: '', disabled: true}],
     });
 
     this.typeproblemeService.obtenirTypesProbleme()
@@ -27,8 +33,18 @@ export class ProblemeComponent implements OnInit{
   }
 
   ngOnInit(): void {
+    this.notification = "nePasMeNotifier";
+    this.isNotificationEnabled = false;
   }
 
+  onNotificationChange(): void {
+    if (this.notification === 'meNotifier') {
+      this.isNotificationEnabled = true;
+    } else {
+      this.isNotificationEnabled = false;
+    }
+  }
+  
   save(): void{}
 
 }
